f5098438c5dc00dd955df1a458c8aa2c3154fcdd
gadsbytom <georgtom734@gmail.com>
2021-06-03 10:36:46 +0200

    notebook commentary updated

--- deep_lm.py
+++ deep_lm.py
@@ -1,5 +1,13 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding, BatchNormalization, Flatten, Bidirectional, LSTM, Dropout
+from tensorflow.keras.layers import (
+    Dense,
+    Embedding,
+    BatchNormalization,
+    Flatten,
+    Bidirectional,
+    LSTM,
+    Dropout,
+)
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.utils import to_categorical
 from sklearn.datasets import fetch_20newsgroups
@@ -7,58 +15,69 @@ import numpy as np
 import re
 
 
-#grab the text
-data = fetch_20newsgroups(remove=['headers', 'footers'])
-text = data['data']
+# grab the text
+data = fetch_20newsgroups(remove=["headers", "footers"])
+text = data["data"]
 
-#create a continuous list of words
-all_data = ' '.join([' '.join(re.findall('(?u)\\b[a-zA-Z]*\\b',article.lower())) for article in text]).split()
+# create a continuous list of words
+all_data = " ".join(
+    [" ".join(re.findall("(?u)\\b[a-zA-Z]*\\b", article.lower())) for article in text]
+).split()
 
-#work out the vocab list and size of vocab
+# work out the vocab list and size of vocab
 vocab_list = sorted(list(set(all_data)))
 n_vocab = len(vocab_list)
 
-#translate words to numbers
+# translate words to numbers
 word_to_num = {}
 num_to_word = {}
 for i, word in enumerate(vocab_list):
     num_to_word[i] = word
     word_to_num[vocab_list[i]] = i
 
-#embed the data
+# embed the data
 embedded_data = [word_to_num[word] for word in all_data]
 
-#create the next word guess for each previous 10 words
-X_data= []
+# create the next word guess for each previous 10 words
+X_data = []
 y_data = []
-seq_length=10
-for i in range(len(embedded_data)-seq_length):
-    X_data.append(embedded_data[i:i+seq_length])
-    y_data.append(embedded_data[i+seq_length])
+seq_length = 10
+for i in range(len(embedded_data) - seq_length):
+    X_data.append(embedded_data[i : i + seq_length])
+    y_data.append(embedded_data[i + seq_length])
 
 
-#reshape the X and y data
+# reshape the X and y data
 X = np.array(X_data).reshape(len(X_data), seq_length)
-y = to_categorical(y_data) 
+y = to_categorical(y_data)
 
 
-#build the model
+# build the model
 model = Sequential()
 model.add(Embedding(input_dim=n_vocab, output_dim=32, input_length=seq_length))
 model.add(Dropout(0.2))
-model.add(Bidirectional(LSTM(128, return_sequences=True), merge_mode='sum'))
+model.add(Bidirectional(LSTM(128, return_sequences=True), merge_mode="sum"))
 model.add(LSTM(128))
-model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=1e-3, center=True, scale=True))
-model.add(Dense(y.shape[1], activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(
+    BatchNormalization(axis=-1, momentum=0.99, epsilon=1e-3, center=True, scale=True)
+)
+model.add(Dense(y.shape[1], activation="softmax"))
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-#save best version of the model
-filepath=f"best_weights.hdf5"
-early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=15, min_delta=0.0001) 
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True, mode='max')
+# save best version of the model
+filepath = f"best_weights.hdf5"
+early_stop = EarlyStopping(
+    monitor="val_loss", mode="min", verbose=1, patience=15, min_delta=0.0001
+)
+checkpoint = ModelCheckpoint(
+    filepath,
+    monitor="val_loss",
+    verbose=1,
+    save_best_only=True,
+    save_weights_only=True,
+    mode="max",
+)
 callbacks = [early_stop, checkpoint]
 
 # fit the model
 model.fit(X, y, epochs=20, batch_size=512, validation_split=0.2, callbacks=callbacks)
-
-
